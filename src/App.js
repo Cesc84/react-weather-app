e1d3af78f352c3316d2713f4b45d20d21103f21a
francesco <francesco@francescos-iMac.local>
2019-12-20 16:35:19 +0000

    weather component jsx, min max function

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Weather from './component/weather.component';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'weather-icons/css/weather-icons.css';
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
